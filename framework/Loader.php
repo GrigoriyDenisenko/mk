6b8a56ba0c64c6f6c34d21bea08fb58471a301de
Grigoriy Denisenko <Grigoriy.Denisenko@gmail.com>
2016-02-19 22:16:52 +0200

    add comments to router

--- framework/Loader.php
+++ framework/Loader.php
@@ -4,51 +4,69 @@
  */
 class Loader{
 
-    /**
-     * Статическая переменная, в которой мы будем хранить экземпляр класса @var Loader
-     * Паттерн Singleton
-     */
-    protected static $instance = null;
-
-
+	/**
+	* Статическая переменная, в которой мы будем хранить экземпляр класса @var Loader
+	* Паттерн Singleton
+	*/
+	protected static $instance = null;
+     
+    // массив - сисок дирректорий используемых как пространство имён
+	// ключи массива содержат префикс пространства имён
 	protected static $namespaces = array();
 
-	public static function getInstance(){
+    public static function getInstance(){
         /**
-         * Статическая функция, которая возвращает экземпляр класса или создает новый при необходимости
-         * @return Loader
-         */
+        * Статическая функция, которая возвращает экземпляр класса или создает новый при необходимости
+        * @return Loader
+        */
         // проверяем актуальность экземпляра
-		if(empty(self::$instance)){
-            // создаем новый экземпляр
-			self::$instance = new self();
-		}
+        if(empty(self::$instance)){
+          // создаем новый (и единственный) экземпляр себя
+	      self::$instance = new self();
+	    }
         // возвращаем созданный или существующий экземпляр
-		return self::$instance;
-	}
+	return self::$instance;
+    }
 
     private function __construct(){
         // Init
         // регистрация собственного автозагрузчика в стеке автозагрузки (вместо __autoload())
         // стек функций автозагрузки - это массив, элементами которого являются автозагрузчики.
         // Порядок автозагрузчиков соответствует порядку их регистрации, при помощи spl_autoload_register.
-        // echo __CLASS__;
+		// вызов framework/Loader.php регистрирует загрузчик 
+        // в данном случае мы перевели вызовы на себя (__CLASS__ = Loader)
         spl_autoload_register(array(__CLASS__, 'load'));
+		// $obj = new $ClassName теперь пойдёт через Loader::load($ClassName)
+		
         spl_autoload_register(array(__CLASS__, 'addNamespace'));
         // можно ещё добавить выгрузку через  spl_autoload_unregister()
     }
 
-
-	public static function load($classname){
-        // echo "--------load";
+	/**
+     * Загружаем переданный через переменную $classname класс.
+     *
+     * @param string $classname вызываемый класс
+	 * $app = new \Framework\ClassDir\CassName - 
+	 * передаст \Framework\ClassDir\CassName через $classname
+	 * загрузится CassName.php находяшийся ниже текущего каталога
+	 * /var/www.../текущий_каталог="Framework"/ClassDir/CassName.php
+     */
+    
+    public static function load($classname){
+        echo '<pre>';
+        echo "load ".$classname."\n";
+		// убираем алиас Framework, заменив его на путь к текущему каталогу
 		$path = str_replace('Framework','',$classname);
 		$path = __DIR__ . str_replace("\\","/", $path) . '.php';
+        echo "include_once ".$path;
         // может для windows использовать вместо "/" DIRECTORY_SEPARATOR
 		if(file_exists($path)){
 			include_once($path);
 		}
-	}
-
+    }
+    
+	/**
+     */
     public static function addNamespace($classname){
         //@TODO:
         foreach ($this->namespaces as $name => $dir_path){
@@ -63,26 +81,31 @@ class Loader{
         }
     }
 
-	private function __clone(){
+    private function __clone(){
         // lock
         /**
          * Закрываем доступ к функции вне класса.
          * Паттерн Singleton не допускает вызов этой функции вне класса
          **/
-	}
+    }
 
-	public static function addNamespacePath($namespace, $path){
-        //@TODO:
-        // echo "   namespace=".$namespace."," ;
-        // echo "   path=".$path."," ;
+	/**
+    * добавляем в массив соответствие физических директорий - префиксу пространства имён.
+    *
+    * @param string $namespace - префикс пространства имён.
+    * @param string $path - директория для файлов классов из пространства имён.
+    */
+    public static function addNamespacePath($namespace, $path){
+        echo "   namespace=".$namespace."," ;
+        echo "   path=".$path."," ;
         if(is_dir($path)){
             $namespace = rtrim($namespace,'\\');
-            // echo "   new namespace=".$namespace."," ;
+            echo "   new namespace=".$namespace."," ;
             self::$namespaces[$namespace] = $path;
         }
         // $ldr = self::getInstance();
         // $ldr->namespaces[$name] = $path;
-	}
+    }
 
 }
 
--- framework/Router/Router.php
+++ framework/Router/Router.php
@@ -6,35 +6,63 @@ namespace Framework\Router;
 class Router{
     /**
      * @var array
+	 * массив - карта маршрутов, кот. формируется 
+	 * при создании объекта из класса Router
      */
     protected static $map = array();
     /**
      * Class construct
      */
     public function __construct($routing_map = array()){
+		
+		// переданный массив записывается в нашу карту маршрутов
+		// например: app\config\routes.php
+		echo "construct route";
         self::$map = $routing_map;
     }
     /**
      * Parse URL
-     *
+     * определим по нашей карте, что делать с переданным URL
      * @param $url
      */
-    public function parseRoute($url){
-        $route_found = null;
+    public function parseRoute($url = ''){
+		
+		$url = empty($url) ? $_SERVER['REQUEST_URI'] : $url;
+        
+		$route_found = null;
         echo "<pre>";
+        echo "parse Url: ".$url.'<br />';
+		$url=preg_replace('~^\\/index.php$~i', '/', $url);
+		
         foreach(self::$map as $route){
+			// echo "---- Get pattern from MapArray:<br />";
+			// print_r($route);
             $pattern = $this->prepare($route);
-            echo $pattern.'<br />';
+			// echo "Prepared pattern: ".$pattern.'<br />';
+		
             if(preg_match($pattern, $url, $params)){
+                // echo 'find and send to array $params: <br />';
+     			// print_r($params);
                 // Get assoc array of params:
                 preg_match($pattern, str_replace(array('{','}'), '', $route['pattern']), $param_names);
+				// echo " get names of params:  <br />" ;
+     			// print_r($param_names);
+				// $params[0] - теперь содержит всю переданную в URL строку
+				// $params[1] - идентификатор (из {id})
+				// каждый элемент массива $params обработаем через urldecode()
                 $params = array_map('urldecode', $params);
                 $params = array_combine($param_names, $params);
                 array_shift($params); // Get rid of 0 element
+				// первый элемент массива убрали, остался:
+				// $params[id] = "переданный идентификатор"
+				// echo " after shifting:  <br />" ;
+     			// print_r($params);
                 $route_found = $route;
                 $route_found['params'] = $params;
+				// к карте MapArray добавился елемент ['params'] со значением переданного идентификатора
                 break;
             }
+			else{ echo $pattern." # ".$url;}
         }
         return $route_found;
     }
@@ -42,6 +70,7 @@ class Router{
         // @TODO: Your code...
     }
     private function prepare($route){
+		// "{набор_символов}" ("{id}") - меняем на "([\w\d_]+)" (любая последовательность букв, цифр и подчеркивания)
         $pattern = preg_replace('~\{[\w\d_]+\}~Ui','([\w\d_]+)', $route['pattern']);
         $pattern = '~^'. $pattern.'$~';
         return $pattern;
